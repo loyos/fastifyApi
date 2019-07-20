57d930875920d052b1b0bd87ace9e0a2a2bccde4
unknown <loyenrique1@gmail.com>
2019-07-20 15:31:19 +0200

    added post bash renaming

---
+++ tools/test.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{ "title": "Z5", "brand": "blablacarrobonito", "price": "8 000", "age": 10, "services": { "BmW HK": "21/04/2016" } }' http://localhost/api/cars
