07ba1c5011a71121241e9b833d071f9be08083fe
unknown <loyenrique1@gmail.com>
2019-09-07 14:00:44 +0200

    added curl to point to server

--- tools/addIptest.sh
+++ tools/addIptest.sh
@@ -1,2 +1,2 @@
 Now_hourly=$(date +%d-%b-%H-%M)
-curl -X POST -H "Content-Type: application/json" -d '{ "ip": "Z5", "dateTime": "'$Now_hourly'"}' http://localhost/api/ip
+curl -X POST -H "Content-Type: application/json" -d '{ "ip": "Z5", "dateTime": "'$Now_hourly'"}' http://tumnago.es/api/ip
